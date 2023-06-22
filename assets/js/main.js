3820dcc5b177e448a61cfa5b4ff7b1a317d9a194
blackAdamCambodia <blackadam2000kh@gmail.com>
2023-06-22 19:29:29 +0700

    use bootstrap class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,41 @@
 const apiKey = "45131a06c136e6a3e9a287575331aa01";
-const apiUrl ="https://api.openweathermap.org/data/2.5/weather?&unit=metric&q=dhaka";
-
-async function checkWeather ( ){
-    const response =await fetch(apiUrl+`&appid=$(apiKey)`);
-    var data = await response.json();
-    console.log(data);
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = data.main.temp;
-    document.querySelector('.humidity').innerHTML = data.main.humidity;
-    document.querySelector('.wind').innerHTML = data.wind.speed;
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+const weatherIcon = document.querySelector('.weather-icon')
+async function checkWeather (city ){
+    const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
+    if(response.status ==404){
+        document.querySelector('.error').style.display='block';
+        document.querySelector('.weather').style.display='none';
+    }else {
+        var data = await response.json();
+        console.log(data);
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+        document.querySelector('.wind').innerHTML = data.wind.speed + 'Km/h';
+        if(data.weather[0].main=='Clouds') {
+            weatherIcon.src='/assets/images/clouds.png';
+        } 
+        else if(data.weather[0].main=='Clear') {
+            weatherIcon.src='/assets/images/clear.png';
+        }
+        else if(data.weather[0].main=='Rain') {
+            weatherIcon.src='/assets/images/rain.png';
+        }
+        else if(data.weather[0].main=='Drizzle') {
+            weatherIcon.src='/assets/images/drizzle.png';
+        }
+        else if(data.weather[0].main=='Mist') {
+            weatherIcon.src='/assets/images/mist.png';
+        }
+        document.querySelector('.weather').style.display='block'
+        document.querySelector('.error').style.display='none';
+    }
+   
 
 }
-checkWeather ( )
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(searchBox.value)
+})
